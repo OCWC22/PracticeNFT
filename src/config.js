b01f51d590e7a989b59479d6874c05ee9e944a7a
William Chen <57119977+OCWC22@users.noreply.github.com>
2022-01-22 21:29:42 -0800

    lame cat modification

--- src/config.js
+++ src/config.js
@@ -27,12 +27,12 @@ const layerConfigurations = [
     growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Skins" },
+      { name: "Faces" },
+      { name: "Clothes" },
+      { name: "Outline" },
+      { name: "Head" },
+      { name: "Accessory" },
     ],
   },
 ];
@@ -78,7 +78,9 @@ const background = {
   default: "#000000",
 };
 
-const extraMetadata = {};
+const extraMetadata = {
+
+};
 
 const rarityDelimiter = "#";
 
